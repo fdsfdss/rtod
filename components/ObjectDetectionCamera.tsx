82aa4811dbae941496320993b4f95d594b1b7379
fdsfdss <148657533+fdsfdss@users.noreply.github.com>
2023-11-02 13:29:38 +0000

    Update ObjectDetectionCamera.tsx and sw.js.map

--- components/ObjectDetectionCamera.tsx
+++ components/ObjectDetectionCamera.tsx
@@ -12,6 +12,7 @@ interface WebcamComponentProps {
   width: number; // Add width prop with the appropriate type
   height: number; // Add height prop with the appropriate type // Adjust the types as needed
   resizeCanvasCtx?: (ctx: CanvasRenderingContext2D, targetWidth: number, targetHeight: number, inPlace?: boolean) => CanvasRenderingContext2D; // Adjust the return type as needed
+  changeModelResolution: (width: number, height: number) => void;
 
 }
 
